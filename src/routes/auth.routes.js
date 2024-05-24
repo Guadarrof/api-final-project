34335751efd8983176773fbc0ff073012d9239f7
“Guada” <guadalupearroferia@gmail.com>
2024-05-24 23:32:55 +1000

    ADD upload form validations

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -6,15 +6,49 @@ import { validationErrorRes } from "../middleware/validations.js";
 const route = express.Router()
 
 route
-    .post("/create", 
+    .post("/createProduct", 
         [
-            body("userName").isString().isLength({min:1}).withMessage("El nombre de usuario es requerido"),
-            body("email").isEmail().withMessage("El email es requerido"),
-            body("password")
-            .isString().isLength({min:1}).withMessage("La password es requerida.")
-            .bail()
-            .isLength({ min: 8}).withMessage("La password debe tener al menos 8 caracteres."),
-            validationErrorRes
+            body("productName")
+                .isString()
+                .isLength({min:1})
+                .withMessage("El nombre del producto es requerido"),
+            body("price")
+                .isNumeric()
+                .isLength({min:1})
+                .withMessage("El producto requiere un precio")
+                .bail()
+                .isInt()
+                .withMessage("El precio debe ser mayor a 0"),
+            body("shortDescription")
+                .isString()
+                .isLength({min:1})
+                .withMessage("El producto requiere una descripcion")
+                .bail()
+                .isLength({min: 15, max:100})
+                .withMessage("La descripcion debe contener entre 15 y 100 caracteres"),
+            body("longDescription")
+                .isString()
+                .isLength({min:1})
+                .withMessage("El producto requiere una descripcion")
+                .bail()
+                .isLength({min: 50, max:300})
+                .withMessage("La descripcion debe contener entre 50 y 300 caracteres"),
+            body("category")
+                .isString()
+                .isLength({min:1})
+                .withMessage("El producto requiere una categoria"),
+            body("brand")
+                .isString()
+                .isLength({min:1})
+                .withMessage("El producto requiere una marca"),
+            body("stock")
+                .isNumeric()
+                .isLength({min:1})
+                .withMessage("El producto requiere un valor")
+                .bail()
+                .isInt()
+                .withMessage("El stock debe ser mayor a 0"),
+                validationErrorRes
         ],
      uploadProduct)
 
